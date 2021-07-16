9e7eae75cc0deab4e617b8b0a6407a16d4836030
isaskou <isabel.skou@yahoo.fr>
2021-07-16 13:52:17 +0200

    update project

---
+++ VaccinationManager.Tools/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationManager.Tools
+{
+    public class PasswordHasher
+    {
+        public static byte[] Hashing<T>(T userModel, string password, Func<T, string> saltSelector)
+        {
+            string salt = saltSelector(userModel);
+            string result = salt.Substring(0, salt.Length / 2) + password + salt.Substring(salt.Length / 2);
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Encoding.UTF8.GetBytes(result));
+            }
+        }
+    }
+}
