48ed7b724ec4781e8541a9dae5858b6395994f41
isaskou <isabel.skou@yahoo.fr>
2021-07-16 17:28:31 +0200

    add Passwordhasher

--- VaccinationManager.Tools/PasswordHasher.cs
+++ VaccinationManager.Tools/PasswordHasher.cs
@@ -9,11 +9,11 @@ namespace VaccinationManager.Tools
 {
     public class PasswordHasher
     {
-        public static byte[] Hashing<T>(T userModel, string password, Func<T, string> saltSelector)
+        public static byte[] Hashing<T>(T model, string password, Func<T, string> saltSelector)
         {
-            string salt = saltSelector(userModel);
+            string salt = saltSelector(model);
             string result = salt.Substring(0, salt.Length / 2) + password + salt.Substring(salt.Length / 2);
-            using (SHA256 sha256 = SHA256.Create())
+            using(SHA256 sha256 = SHA256.Create())
             {
                 return sha256.ComputeHash(Encoding.UTF8.GetBytes(result));
             }
