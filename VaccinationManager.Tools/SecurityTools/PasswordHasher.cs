dc851f59e32f235a4d1083757d3235f1157e0b7c
isaskou <isabel.skou@yahoo.fr>
2021-07-23 13:31:28 +0200

    add PasswordHasher

---
+++ VaccinationManager.Tools/SecurityTools/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccinationManager.Tools.SecurityTools
+{
+    public static class PasswordHasher
+    {
+        public static byte[] HashMe(string passwordIn)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(passwordIn);
+            byte[] result;
+
+            SHA512 shaM = new SHA512Managed();
+
+            result = shaM.ComputeHash(data);
+            return result;
+                }
+    }
+}
